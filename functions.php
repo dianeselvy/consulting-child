5e54aed20e90cca47f539b20333bf0998eced313
Dann Frey <dann.frey@me.com>
2017-11-02 20:03:27 -0700

    Whatevrer
    SORRY!

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
     // engueue parent styles
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-    wp_enqueue_sytle( 'parent-style', get_template_directory_uri().'/styles/style-responsive.css');
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/styles/style-responsive.css');
     
     // enqueue child styles
 	wp_enqueue_style('child-theme', get_stylesheet_directory_uri() .'/style.css', array('parent-theme'));
