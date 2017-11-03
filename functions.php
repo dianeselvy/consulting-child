7c0dcc0872223752d48aacac68d028eb3786b2c0
Dann Frey <dann.frey@me.com>
2017-11-02 19:57:16 -0700

    call to style-responsive

--- functions.php
+++ functions.php
@@ -3,8 +3,10 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
-
+function enqueue_parent_style() {
+    wp_enqueue_sytle( 'parent-style', get_template_directory_uri().'/styles/style-responsive.css');
+}
 	// Register theme stylesheets
 	wp_register_style( 'consulting-thinkup-responsive', get_template_directory_uri() . '/styles/style-responsive.css', '', $consulting_thinkup_theme_version );
