97722fdc9c81cddea0296b5caf6d40c3c27e429d
Dann Frey <dann.frey@me.com>
2017-11-02 20:02:11 -0700

    I think I fixed it
    enqeue styles...maybe.

--- functions.php
+++ functions.php
@@ -3,10 +3,13 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
+    // engueue parent styles
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-}
-function enqueue_parent_style() {
     wp_enqueue_sytle( 'parent-style', get_template_directory_uri().'/styles/style-responsive.css');
+    
+    // enqueue child styles
+	wp_enqueue_style('child-theme', get_stylesheet_directory_uri() .'/style.css', array('parent-theme'));
+	wp_enqueue_style('child-theme', get_stylesheet_directory_uri() .'/styles/style-responsive.css', array('parent-theme'));
 }
 	// Register theme stylesheets
 	wp_register_style( 'consulting-thinkup-responsive', get_template_directory_uri() . '/styles/style-responsive.css', '', $consulting_thinkup_theme_version );
