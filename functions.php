09161569d396937c2976cb1939da700f8b9709e1
Dann Frey <dann.frey@me.com>
2017-11-07 12:46:12 -0800

    Fixed. Sorry. my bad.
    A comma got me.

--- functions.php
+++ functions.php
@@ -5,11 +5,11 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
     // engueue parent styles
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/styles/style-responsive.css');
+    wp_enqueue_style( 'parent-style-responsive', get_template_directory_uri().'/styles/style-responsive.css');
     
-    // enqueue child styles
-	wp_enqueue_style('child-style', get_stylesheet_directory_uri() .'/style.css', array('consulting-thinkup-style'));
-	wp_enqueue_style('child-style-responsive' get_stylesheet_directory_uri() .'/styles/style-responsive.css', array('consulting-thinkup-responsive'));
+   // enqueue child styles
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() .'/style.css', array('parent-style'));
+    wp_enqueue_style('child-style-responsive', get_stylesheet_directory_uri() .'/styles/parent-style-responsive.css', array('consulting-thinkup-responsive'));
 }
-	// Register theme stylesheets
-	wp_register_style( 'consulting-thinkup-responsive', get_template_directory_uri() . '/styles/style-responsive.css', '', $consulting_thinkup_theme_version );
+    // Register theme stylesheets
+    //wp_register_style( 'consulting-thinkup-responsive', get_template_directory_uri() . '/styles/style-responsive.css', '', $consulting_thinkup_theme_version );
